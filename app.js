c23c495d3b5e4f0d412f466f46b5766f61ed9e1e
Kyrie <kyrie.meng@ximalaya.com>
2018-12-17 17:05:54 +0800

    ..

---
+++ app.js
@@ -0,0 +1,94 @@
+
+
+function registerServiceWorker() {
+  return navigator.serviceWorker.register('/sw.js', {scope: '/'})
+    .then(registration => {
+      console.log('Service worker successfully registered.');
+      return registration;
+    })
+    .catch(err => {
+      console.error('Unable to register service worker.', err);
+    });
+}
+
+
+// 获取发送通知权限
+let promiseChain = new Promise((resolve, reject) => {
+  const permissionPromise = Notification.requestPermission(result => {
+    resolve(result);
+  });
+  if (permissionPromise) {
+    permissionPromise.then(resolve);
+  }
+}).then(result => {
+  if (result === 'granted') {
+    registerServiceWorker().then(registration => {
+      // 
+      registration.showNotification('Hello World!');
+    });
+  } else {
+    registerServiceWorker();
+  }
+});
+
+
+
+
+
+
+
+
+function imgLoad(imgJSON) {
+  // return a promise for an image loading
+  return new Promise(function (resolve, reject) {
+    var request = new XMLHttpRequest();
+    request.open('GET', imgJSON.url);
+    request.responseType = 'blob';
+
+    request.onload = function () {
+      if (request.status == 200) {
+        var arrayResponse = [];
+        arrayResponse[0] = request.response;
+        arrayResponse[1] = imgJSON;
+        resolve(arrayResponse);
+      } else {
+        reject(Error('Image didn\'t load successfully; error code:' + request.statusText));
+      }
+    };
+
+    request.onerror = function () {
+      reject(Error('There was a network error.'));
+    };
+
+    // Send the request
+    request.send();
+  });
+}
+
+var imgSection = document.querySelector('section');
+console.log(Gallery);
+
+window.onload = function () {
+
+  // load each set of image, alt text, name and caption
+  for (var i = 0; i <= Gallery.images.length - 1; i++) {
+    imgLoad(Gallery.images[i]).then(function (arrayResponse) {
+
+      var myImage = document.createElement('img');
+      var myFigure = document.createElement('figure');
+      var myCaption = document.createElement('caption');
+      var imageURL = window.URL.createObjectURL(arrayResponse[0]);
+
+      myImage.src = imageURL;
+      myImage.setAttribute('alt', arrayResponse[1].alt);
+      myCaption.innerHTML = '<strong>' + arrayResponse[1].name + '</strong>: Taken by ' + arrayResponse[1].credit;
+
+      imgSection.appendChild(myFigure);
+      myFigure.appendChild(myImage);
+      myFigure.appendChild(myCaption);
+
+    }, function (Error) {
+      console.log(Error);
+    });
+  }
+};
---
+++ sw.js
@@ -0,0 +1,79 @@
+var verison = '1.0.1';
+
+// sw.js
+self.addEventListener('install', function (event) {
+  // event.waitUntil(self.skipWaiting());
+  event.waitUntil(
+
+    // 创建一个缓存
+    caches.open('v1').then(function (cache) {
+      
+      // 添加要缓存的资源
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/style.css',
+        '/app.js',
+        '/image-list.js',
+        '/star-wars-logo.jpg',
+        '/gallery/bountyHunters.jpg',
+        '/gallery/myLittleVader.jpg',
+        '/gallery/snowTroopers.jpg'
+      ]);
+    })
+  );
+});
+
+
+// sw.js 管理请求
+this.addEventListener('fetch', function (event) {
+  event.respondWith(
+    caches.match(event.request).then(function () {
+      return fetch(event.request).then(function (response) {
+        return caches.open('v1').then(function (cache) {
+          self.registration.showNotification(`缓存了资源`);
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    }).catch(function () {
+      return caches.match('/sw-test/gallery/myLittleVader.jpg');
+    })
+  );
+});
+
+
+self.addEventListener('activate', function (event) {
+  // sw.js 向用户发送通知
+  var cacheWhitelist = [];
+  self.registration.showNotification('进入激活状态！');
+
+  // 需要在install中配合 event.waitUntil(self.skipWaiting());
+  event.waitUntil(self.clients.claim());
+  event.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (cacheWhitelist.indexOf(key) === -1) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+
+
+self.addEventListener('push', function (event) {
+  event.waitUntil(
+    self.registration.showNotification &&
+    self.registration.showNotification('some message') 
+  );
+});
+
+self.addEventListener('notificationclick', function (event) {
+  event.notification.close();
+});
+
+self.addEventListener('notificationclose', function (event) {
+  // do something
+});
