9aaf7b0f514de93862c6ce2a39853e3fb74f077b
Kyrie <kyrie.meng@ximalaya.com>
2018-12-17 19:49:04 +0800

    ..

--- sw.js
+++ sw.js
@@ -26,42 +26,55 @@ self.addEventListener('install', function (event) {
 
 
 // sw.js 管理请求
-// this.addEventListener('fetch', function (event) {
-//   event.respondWith(
-//     caches.match(event.request).then(function () {
-//       console.log('sdf', event.request);
-      
-//       return fetch(event.request).then(function (response) {
-//         return caches.open(VERSION).then(function (cache) {
-//           console.log('cache', cache);
-          
-//           self.registration.showNotification(`缓存了资源`);
-//           cache.put(event.request, response.clone());
-//           return response;
-//         });
-//       });
-//     }).catch(function (err) {
-//       console.log(err);
-      
-//       return caches.match('/pwa/gallery/myLittleVader.jpg');
-//     })
-//   );
-// });
+this.addEventListener('fetch', function (event) {
+   event.respondWith(
+    caches.match(event.request).then(function (response) {
+      // 来来来，代理可以搞一些代理的事情
+
+      // 如果 Service Worker 有自己的返回，就直接返回，减少一次 http 请求
+      if (response) {
+        return response;
+      }
+
+      // 如果 service worker 没有返回，那就得直接请求真实远程服务
+      var request = event.request.clone(); // 把原始请求拷过来
+      return fetch(request).then(function (httpRes) {
+
+        // http请求的返回已被抓到，可以处置了。
+
+        // 请求失败了，直接返回失败的结果就好了。。
+        if (!httpRes || httpRes.status !== 200) {
+          return httpRes;
+        }
+
+        // 请求成功的话，将请求缓存起来。
+        var responseClone = httpRes.clone();
+        caches.open(VERSION).then(function (cache) {
+          cache.put(event.request, responseClone);
+        });
+
+        return httpRes;
+      });
+    }).catch(function () {
+      return caches.match('/pwa/gallery/myLittleVader.jpg');
+    })
+   );
+});
 
 // 捕获请求并返回缓存数据
-self.addEventListener('fetch', function (event) {
-  event.respondWith(caches.match(event.request).catch(function () {
-    return fetch(event.request);
-  }).then(function (response) {
-    caches.open(VERSION).then(function (cache) {
-      self.registration.showNotification(`缓存了资源`);
-      cache.put(event.request, response);
-    });
-    return response.clone();
-  }).catch(function () {
-    return caches.match('/pwa/gallery/myLittleVader.jpg');
-  }));
-});
+// self.addEventListener('fetch', function (event) {
+//   event.respondWith(caches.match(event.request).catch(function () {
+//     return fetch(event.request);
+//   }).then(function (response) {
+//     caches.open(VERSION).then(function (cache) {
+//       self.registration.showNotification(`缓存了资源`);
+//       cache.put(event.request, response);
+//     });
+//     return response.clone();
+//   }).catch(function () {
+//     return caches.match('/pwa/gallery/myLittleVader.jpg');
+//   }));
+// });
 
 
 self.addEventListener('activate', function (event) {
