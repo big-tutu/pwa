1ea2c664a94e376484c7ec920a94d66a152632f5
Kyrie <kyrie.meng@ximalaya.com>
2018-12-17 17:37:58 +0800

    修改路径

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 ## PWA
+
+Demo 首页 <a href="https://big-tutu.github.io/pwa/index.html" rel="nofollow">index.html</a>
--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 
 
 function registerServiceWorker() {
-  return navigator.serviceWorker.register('/sw.js', {scope: '/'})
+  return navigator.serviceWorker.register('./sw.js', {scope: './'})
     .then(registration => {
       console.log('Service worker successfully registered.');
       return registration;
